403652cfed499fe1d6443691da315848ea0a305f
oneeyeyu <155784504+oneeyeyu@users.noreply.github.com>
2025-07-31 18:44:02 +0900

    課題提出

---
+++ kadai007/associative-array.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎課題</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $Array = ['name' => 'onion', 'aprice' => 200, 'weght' => '160'];
+
+       // 連想配列の値を出力する
+       print_r($Array);
+
+       // 改行する
+    //    echo '<br>';
+
+    //    // 'age'というキーの値を更新する
+    //    $Array['age'] = 37;
+
+    //    // 新しく要素（キーと値）を追加する
+    //    $Array['address'] = '東京都';
+
+    //    // 連想配列の値を出力する
+    //    print_r($Array);
+       
+    //      echo'<br>';
+    //      echo $Array['gender'];
+        ?>
+   </p>
+</body>
+
+</html>
